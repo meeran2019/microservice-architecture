5be84e207ebbe4c542ebbc9517b212a7ed9453a5
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 23:05:21 +0530

    Initial Commit

--- terraform/general-ec2.tf
+++ terraform/general-ec2.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "general-ec2" {
   depends_on = [aws_eks_node_group.worker-node-group]
   user_data = templatefile("bootstrap.tpl", {
     access_key = var.access_key,
-    secret_key = var.secret_key,
+    secret_key = var.secret_key
   })
 }
