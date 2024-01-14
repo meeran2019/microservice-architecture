7afd209293fd71943a520d80957398554bcff4ac
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 17:29:14 +0530

    Initial Commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_instance_profile" "ec2_profile_1" {
-  name = "ec2_profile_1"
+resource "aws_iam_instance_profile" "ec2profile" {
+  name = "ec2profile"
   role = "ec2role "
 }
 
@@ -7,6 +7,6 @@ resource "aws_instance" "general-ec2" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.large"
   user_data = "${file("bootstrap.sh")}"
-  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile_1.name}"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2profile.name}"
   depends_on = [aws_eks_node_group.worker-node-group]
 }
