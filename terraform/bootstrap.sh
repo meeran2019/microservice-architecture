da4ac8f1c13d64a99eda03c15acfc315c85ec4ce
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 22:31:01 +0530

    Initial Commit

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -21,3 +21,8 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$P
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 kubectl version --client
 aws eks update-kubeconfig --region us-east-1 --name eks-cluster
+
+aws configure set aws_access_key_id $access_key
+aws configure set aws_secret_access_key $secret_key
+aws configure set default.region us-east-1
+aws configure set default.output json
