bcd74700998e05fd5a615786db82f9cd2e6960d4
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 13:12:14 +0530

    Initial Commit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,3 +15,16 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
