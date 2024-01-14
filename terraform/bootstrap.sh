bfb1935d5d432484577141849acd684fce677a57
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 21:59:09 +0530

    Initial Commit

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum update –y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
@@ -10,12 +10,6 @@ sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
-terraform -help
-terraform -help plan
 
 kubectl version --client
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl
