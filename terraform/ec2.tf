27cff2ad0ec2d5422d18ba9fd0843f0e8cd630d1
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 17:09:02 +0530

    Initial Commit

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "general-ec2" {
   ami           = "ami-0005e0cfe09cc9050"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   user_data = "${file("bootstrap.sh")}"
   depends_on = [aws_eks_node_group.worker-node-group]
 }
