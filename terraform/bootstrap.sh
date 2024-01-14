d2918b74fff17c44b9c1eea5f81f440b42afbf75
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2024-01-14 14:52:34 +0530

    Initial Commit

---
+++ terraform/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+kubectl version --client
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl.sha256
+sha256sum -c kubectl.sha256
+openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+kubectl version --client
+aws eks update-kubeconfig --region us-east-1 --name eks-cluster
+
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
+  user_data = "${file("bootstrap.sh")}"
+  depends_on = [aws_eks_node_group.worker-node-group]
 }
